e9ab9494071829c5e15abded99abbd44f19b99ce
christopher-1988 <christopher_carnevale@outlook.com>
2023-11-10 08:07:01 -0500

    descarga de cambios

--- routes/login.js
+++ routes/login.js
@@ -2,34 +2,25 @@ const express = require("express");
 const User = require("../schema/user");
 const { jsonResponse } = require("../lib/jsonResponse");
 const getUserInfo = require("../lib/getUserInfo");
+
 const router = express.Router();
 
 router.post("/", async function (req, res, next) {
   const { email, password } = req.body;
-
-  //console.log('data recibida', { email, password })
-
   try {
     let user = new User();
     const userExists = await user.usernameExists(email);
-    //console.log('userExists', userExists)
-
     if (userExists) {
       user = await User.findOne({ email: email });
-      //console.log('user', user)
-
       const passwordCorrect = await user.isCorrectPassword(
         password,
         user.password
       );
 
-      //console.log('passwordCorrect', passwordCorrect)
-
       if (passwordCorrect) {
         const accessToken = user.createAccessToken();
         const refreshToken = await user.createRefreshToken();
 
-        //console.log('todo ok')
         return res.json(
           jsonResponse(200, {
             accessToken,
@@ -38,9 +29,6 @@ router.post("/", async function (req, res, next) {
           })
         );
       } else {
-        //res.status(401).json({ error: "email and/or password incorrect" });
-
-        //console.log('email and/or password incorrect')
         return res.status(401).json(
           jsonResponse(401, {
             error: "email y/o clave incorrecta",
@@ -48,7 +36,6 @@ router.post("/", async function (req, res, next) {
         );
       }
     } else {
-      //console.log("email does not exist");
       return res.status(401).json(
         jsonResponse(401, {
           error: "email no existe",
@@ -56,7 +43,7 @@ router.post("/", async function (req, res, next) {
       );
     }
   } catch (err) {
-    //console.log("err",err);
+    console.error(err);
   }
 });
 
