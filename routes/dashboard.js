14e45c1fcc457463ce269b18dd592492a10aeb61
christopher-1988 <christopher_carnevale@outlook.com>
2023-11-14 07:44:31 -0500

    fit[general]correcion de errores

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,14 +4,11 @@ const { jsonResponse } = require("../lib/jsonResponse");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    return 'hola';
-    //const resultados = await PlatosVendidosSchema.obtenerPlatosMasVendidos();
-    //res.json(resultados);
+  return "hola";
 });
 
 router.get("/chef/:id", async function (req, res) {
   const id = req.params.id;
-  //id_plato, id_chef, fecha
   try {
     const data = await PlatoSchema.dashboard({ id_chef: id });
 
@@ -22,6 +19,7 @@ router.get("/chef/:id", async function (req, res) {
       })
     );
   } catch (err) {
+    console.log(err);
     return res.status(500).json(
       jsonResponse(500, {
         error: "Error al obtener el plato",
@@ -29,24 +27,37 @@ router.get("/chef/:id", async function (req, res) {
     );
   }
 });
-
 // Ruta para Platos más vendidos
 router.get("/platosVendidos", async (req, res) => {
-    const resultados = await PlatosVendidosSchema.obtenerPlatosVendidos();
-    res.json(resultados);
+  const data = await PlatosVendidosSchema.obtenerPlatosVendidos();
+
+  return res.json(
+    jsonResponse(200, {
+      data,
+      recordsTotal: data.length,
+    })
+  );
 });
-  
 // Ruta para Platos más vendidos del chef
 router.get("/platosVendidos/:id_chef", async (req, res) => {
-    const { id_chef } = req.params;
-    const resultados = await PlatosVendidosSchema.obtenerPlatosVendidos(id_chef);
-    res.json(resultados);
+  const { id_chef } = req.params;
+  const data = await PlatosVendidosSchema.obtenerPlatosVendidos(id_chef);
+  return res.json(
+    jsonResponse(200, {
+      data,
+      recordsTotal: data.length,
+    })
+  );
 });
-  
 // Ruta para Chef con más ventas
 router.get("/ventasPorChef", async (req, res) => {
-    const resultados = await PlatosVendidosSchema.obtenerVentasPorChef();
-    res.json(resultados);
+  const data = await PlatosVendidosSchema.obtenerVentasPorChef();
+  return res.json(
+    jsonResponse(200, {
+      data,
+      recordsTotal: data.length,
+    })
+  );
 });
 
 module.exports = router;
